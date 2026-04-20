0ae33869e94c5e97920a17a955a555c30e3de488
ychung333 <166780948+ychung333@users.noreply.github.com>
2026-04-20 15:08:32 -0700

    Update sound.py

--- sound.py
+++ sound.py
@@ -1,14 +1,19 @@
 import pygame
 
+# keep track of what BGM is currently playing
+# this prevents restarting the same music again and again
 current_bgm = None
 
 def init_audio():
+    # initialize the pygame mixer (audio system)
     pygame.mixer.init()
 
 def play_bgm(path, volume=0.5):
     global current_bgm
+
+    # only change music if it's moving screen from title. game and result screen are run same music
     if current_bgm != path:
-        pygame.mixer.music.load(path)
-        pygame.mixer.music.set_volume(volume)
-        pygame.mixer.music.play(-1)
-        current_bgm = path
+        pygame.mixer.music.load(path)        # load the music file
+        pygame.mixer.music.set_volume(volume)  # set volume (0.0 to 1.0)
+        pygame.mixer.music.play(-1)          # play in loop (-1 = infinite loop)
+        current_bgm = path                  # remember current playing BGM
